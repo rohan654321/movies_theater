d05ccb3eb00b791595a76d0dce53a04c09dd5649
rohan654321 <mondalrohan201@gmail.com>
2025-05-14 13:01:04 +0530

    update

--- lib/api.ts
+++ lib/api.ts
@@ -1,6 +1,6 @@
 import type { MovieDetails, MoviesResponse } from "@/types"
 
-const API_KEY = process.env.NEXT_PUBLIC_TMDB_API_KEY
+const API_KEY = process.env.TMDB_API_KEY
 const BASE_URL = "https://api.themoviedb.org/3"
 
 if (!API_KEY) {
